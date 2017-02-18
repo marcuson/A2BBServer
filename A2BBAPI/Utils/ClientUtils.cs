d0ed0dc1fefa0bfb043291ac8f73c1e3a136b893
Leonardo Marcucci <l.marcucci@sintraconsulting.eu>
2017-02-18 10:20:33 +0100

    Add utils to get a client with client_credential and refresh_token flows.

--- A2BBAPI/Utils/ClientUtils.cs
+++ A2BBAPI/Utils/ClientUtils.cs
@@ -13,14 +13,49 @@ namespace A2BBAPI.Utils
         /// <summary>
         /// Get token for API using username and password.
         /// </summary>
+        /// <param name="clientId">The client id.</param>
+        /// <param name="scope">The requested scope.</param>
         /// <param name="username">The username.</param>
         /// <param name="password">The password.</param>
-        /// <returns></returns>
+        /// <returns>A token response.</returns>
         public static TokenResponse GetROClient(string scope, string clientId, string username, string password)
         {
             var disco = DiscoveryClient.GetAsync(Constants.IDENTITY_SERVER_ENDPOINT).Result;
             var client = new TokenClient(disco.TokenEndpoint);
             return client.RequestResourceOwnerPasswordAsync(username, password, scope, new Dictionary<string, string> {
+                { "client_id", clientId }
+            }).Result;
+        }
+
+        /// <summary>
+        /// Get token for API using client credentials.
+        /// </summary>
+        /// <param name="clientId">The client id.</param>
+        /// <param name="scope">The requested scope.</param>
+        /// <param name="secret">The client secret.</param>
+        /// <returns>A token response.</returns>
+        public static TokenResponse GetCCClient(string scope, string clientId, string secret)
+        {
+            var disco = DiscoveryClient.GetAsync(Constants.IDENTITY_SERVER_ENDPOINT).Result;
+            var client = new TokenClient(disco.TokenEndpoint);
+            return client.RequestClientCredentialsAsync(scope, new Dictionary<string, string> {
+                { "client_id", clientId },
+                { "client_secret", secret}
+            }).Result;
+        }
+
+        /// <summary>
+        /// Get token for API using refresh token.
+        /// </summary>
+        /// <param name="clientId">The client id.</param>
+        /// <param name="scope">The requested scope.</param>
+        /// <param name="refreshToken">The refresh token.</param>
+        /// <returns>A token response.</returns>
+        public static TokenResponse GetRTClient(string scope, string clientId, string refreshToken)
+        {
+            var disco = DiscoveryClient.GetAsync(Constants.IDENTITY_SERVER_ENDPOINT).Result;
+            var client = new TokenClient(disco.TokenEndpoint);
+            return client.RequestRefreshTokenAsync(refreshToken, new Dictionary<string, string> {
                 { "client_id", clientId }
             }).Result;
         }
